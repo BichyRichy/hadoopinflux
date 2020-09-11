cb36d8dbe814f38f827739b50ee1a441f9fe5cb6
Richard Gao <rgao@ucsd.edu>
2020-09-11 14:14:29 -0700

    a

--- query.py
+++ query.py
@@ -1,7 +1,11 @@
 import requests
+import json
 url = 'http://graph.t2.ucsd.edu:8086/query?pretty=true&db=hadoop_metrics_db'
+with open('conf.json') as h:
+	header = json.load(h)
 
-q = '&q=SELECT \"value\" FROM \"percent_used\"'
+
+q = '&q=SELECT \"value\" FROM \"missing_blocks\"'
 
 r = requests.get(url+q, headers=header)
 print(r.text)
