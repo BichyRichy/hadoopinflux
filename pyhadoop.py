70b30506ef3d94d0ec873d7ebc135bb8a78f5ea4
Richard Gao <rgao@ucsd.edu>
2020-09-10 19:42:39 -0700

    m

--- pyhadoop.py
+++ pyhadoop.py
@@ -8,12 +8,17 @@ url = 'http://graph.t2.ucsd.edu:8086/write?db=hadoop_metrics_db'
 header = {'Authorization': 'Token hadoop_writer:Hadoop3r'}
 
 for key in dirs:
-	out = subprocess.Popen(['hadoop', 'fs', '-du', dirs[key]],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	stdout,stderr = out.communicate()
+	fsdu = subprocess.Popen(['hadoop', 'fs', '-du', dirs[key]],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	stdout,stderr = fsdu.communicate()
 	for line in stdout.splitlines():
 		try:
 			size, usage, path = line.split()
-			data = 		
+			#data = 		
 		except ValueError:
-			print "Error: Too many values"
+			#print "Error: Too many values"
 		
+fs = subprocess.Popen(['hadoop', 'fs', '-df'],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+stdout, stderr = fs.communicate()
+
+for line in stdout.splitlines():
+	print line
