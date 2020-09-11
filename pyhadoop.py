bef1d1b6e8c6c6988e1749755bd108db18ccb975
Richard Gao <rgao@ucsd.edu>
2020-09-10 21:30:10 -0700

    adf

--- pyhadoop.py
+++ pyhadoop.py
@@ -7,16 +7,6 @@ dirs = {"CMS": "/cms/*", "Phedex": "/cms/phedex/store/*", "Store": "/cms/store/*
 url = 'http://graph.t2.ucsd.edu:8086/write?db=hadoop_metrics_db'
 header = {'Authorization': 'Token hadoop_writer:Hadoop3r'}
 
-for key in dirs:
-	fsdu = subprocess.Popen(['hadoop', 'fs', '-du', dirs[key]],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	stdout,stderr = fsdu.communicate()
-	for line in stdout.splitlines():
-		try:
-			size, usage, path = line.split()
-			#data = 		
-		except ValueError:
-			#print "Error: Too many values"
-			continue
 		
 fs = subprocess.Popen(['hadoop', 'fs', '-df'],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 stdout, stderr = fs.communicate()
@@ -27,3 +17,14 @@ for line in stdout.splitlines():
 		print [fs, size, used, available, perc]
 	except ValueError:
 		continue
+
+for key in dirs:
+	fsdu = subprocess.Popen(['hadoop', 'fs', '-du', dirs[key]],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	stdout,stderr = fsdu.communicate()
+	for line in stdout.splitlines():
+		try:
+			size, usage, path = line.split()
+			#data = 		
+		except ValueError:
+			#print "Error: Too many values"
+			continue
